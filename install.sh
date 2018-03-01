9db99e275c6c22548e20c9e53df94abb87719faf
John Evans <john@grandadevans.com>
2018-03-01 03:47:45 +0000

    Added install script and initial notes

---
+++ install.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Get the IP address
+IP=$(hostname -I)
+
+echo "####################\nYour IP address is: \n# $IP\n####################"
+
+sudo apt-get update
+
+sudo apt-get install socat jq curl avahi-daemon apt-transport-https ca-certificates software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo apt-key fingerprint 0eBFCD88
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+sudo apt-get update
+
+sudo apt-get install docker-ce
+
+sudo apt-cache madison docker-ce
+
+curl -sL https://raw.githubusercontent.com/home-assistant/hassio-build/master/install/hassio_install | bash -
+
+sudo docker run hello-world
+
+echo "\nYour new docker containers are:\n$(docker ps)\n"
+
+echo "\nSuccess! go to\n\nhttp://$(IP):8123\n\n"
