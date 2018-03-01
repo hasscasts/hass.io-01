f45365d3fb6da6e3671bb6cddc9272c808840c3a
hasscasts <36940037+hasscasts@users.noreply.github.com>
2018-03-01 06:00:00 +0000

    Run hass.io script as root

--- install.sh
+++ install.sh
@@ -22,6 +22,6 @@ sudo apt-get update
 
 sudo apt-get install -y docker-ce
 
-curl -sL https://raw.githubusercontent.com/home-assistant/hassio-build/master/install/hassio_install | bash -
+curl -sL https://raw.githubusercontent.com/home-assistant/hassio-build/master/install/hassio_install | sudo bash -
 
 echo -e "\nSuccess! go to\n\nhttp://$IP:8123\n"
