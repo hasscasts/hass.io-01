d9c0acaa7e75e21fe662a79cd2921a3dcc050684
hasscasts <36940037+hasscasts@users.noreply.github.com>
2018-03-01 05:36:26 +0000

    Final clean up

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Get the IP address
-export IP=$(hostname -I)
+export IP="$(echo -e hostname -I | sed -e 's/^[[:space:]]*//' -e 's/[[:space:]]*$//')"
 
 echo -e "####################\nYour IP address is: \n# $IP\n####################"
 
 sudo apt-get update
 
-sudo apt-get install -y socat jq curl avahi-daemon apt-transport-https ca-certificates software-properties-common
+sudo apt-get install -y socat jq avahi-daemon apt-transport-https ca-certificates software-properties-common
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
@@ -23,7 +23,5 @@ sudo apt-get update
 sudo apt-get install -y docker-ce
 
 curl -sL https://raw.githubusercontent.com/home-assistant/hassio-build/master/install/hassio_install | bash -
-
-echo -e "\nYour new docker containers are:\n$(docker ps)\n"
 
 echo -e "\nSuccess! go to\n\nhttp://$IP:8123\n"
